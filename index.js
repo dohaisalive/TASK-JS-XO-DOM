99dd1e8b48f958f39d55ca464b221592d5093e03
dohaisalive <dohaisalive@gmail.com>
2022-04-11 17:10:41 +0300

    initial solution

--- index.js
+++ index.js
@@ -1,36 +1,91 @@
-// ❗️ DON'T TOUCH THESE 2 FUNCTIONs
-// Pre-made function that will fill the button with its number.
-// First button top left is called 1, last button bottom right is 9
+let XorO = true;
+let writeText = "X";
+
 function fillButton(index, text) {
   // This function fills the button of the send index
   document.getElementById(index).innerHTML = text;
 }
-// pre-made a function. You can use this function to present an alert to say someone wins
-function winningAlert(winner) {
-  if(confirm(`Horraaay, ${winner} wins!`)){
-     // The code here will be exectued if you press on OK button that will pop on the window 
+
+/**
+ * THE MAIN FUNCTION
+ * This function gets executed every time the user clicks the button
+ */
+function clickButton(index) {
+  if (!document.getElementById(index).innerHTML) {
+    if (XorO) {
+      writeText = "X";
+    } else {
+      writeText = "O";
+    }
+    fillButton(index, writeText);
+    XorO = !XorO;
+  }
+  checkWinner();
+  checkIfDraw();
+}
+
+function checkWinner() {
+  let element1;
+  let element2;
+  let element3;
+
+  //loop through rows
+  for (let i = 1; i <= 9; i = i + 3) {
+    element1 = document.getElementById(i).innerHTML;
+    element2 = document.getElementById(i + 1).innerHTML;
+    element3 = document.getElementById(i + 2).innerHTML;
+    isThereAWinner(element1, element2, element3);
+  }
+
+  //loop through columns
+  for (let i = 1; i <= 3; i++) {
+    element1 = document.getElementById(i).innerHTML;
+    element2 = document.getElementById(i + 3).innerHTML;
+    element3 = document.getElementById(i + 6).innerHTML;
+    isThereAWinner(element1, element2, element3);
+  }
+
+  //check diagonals
+  element1 = document.getElementById(1).innerHTML;
+  element2 = document.getElementById(5).innerHTML;
+  element3 = document.getElementById(9).innerHTML;
+  isThereAWinner(element1, element2, element3);
+
+  element1 = document.getElementById(3).innerHTML;
+  element2 = document.getElementById(5).innerHTML;
+  element3 = document.getElementById(7).innerHTML;
+  isThereAWinner(element1, element2, element3);
+}
+
+function isThereAWinner(element1, element2, element3) {
+  if (element1 === element2 && element1 === element3 && element1.length !== 0) {
+    winningAlert(`${element1}`);
   }
 }
 
-// SAMPLE CODE: This code fills the 1st and 9th button with X and O initially
-// ❗️ Delete this code once you are done testing
-fillButton(1, "X");
-fillButton(9, "O");
-
-/**
- *
- * THE MAIN FUNCTION
- * This function gets executed every time the user clicks the button
- * Add your code here, since this is going to be your main function
- * That interacts with the UI
- */
-function clickButton(index) {
-  console.log(`Button number ${index} is clicked`);
-  // Your main code here.
+function winningAlert(winner) {
+  if (confirm(`Horraaay, ${winner} wins!`)) {
+    restartGame();
+  }
 }
 
-/**
- * (Optional) It's always a good idea to make a function for every single purpose.
- */
-// function checkWinner
-// function restartGame
+function checkIfDraw() {
+  let counter = 0;
+  for (let i = 1; i <= 9; i++) {
+    if (document.getElementById(i).innerHTML.length !== 0) {
+      counter++;
+    }
+    if (counter === 9) {
+      alert("draw! try again");
+      restartGame();
+    }
+  }
+}
+
+function restartGame() {
+  for (let i = 1; i <= 9; i++) {
+    fillButton(i, "");
+  }
+  writeText = "X";
+  XorO = true;
+}
